df15b32bd57e99bde933598689705973e9304312
Your Name <you@example.com>
2022-07-26 07:22:02 +0000

    [Finish]3-1

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
 end
