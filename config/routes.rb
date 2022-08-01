9b2e70716e89e6fe3aaa2e0b1f157fba0f821541
Your Name <you@example.com>
2022-08-01 00:21:22 +0000

    [Finish]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
+  # .../lists/1 や .../list/3に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
 end
